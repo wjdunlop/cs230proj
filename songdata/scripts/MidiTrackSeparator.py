a498122e37cd66692d88e2603fcc75d4b2a90077
wjdunlop <wjdunlop@users.noreply.github.com>
2020-02-22 17:24:41 -0800

    cut tracks up

--- songdata/scripts/MidiTrackSeparator.py
+++ songdata/scripts/MidiTrackSeparator.py
@@ -13,22 +13,33 @@ for filename in os.listdir(path):
 print(targets[:5])
 
 for fileN in targets:
-
+	print(fileN)
 	thisPath = path + fileN
 	tracks = []
-	thisfile = MidiFile(thisPath, clip = True)
-	for track in thisfile.tracks:
-	    tracks.append(track)
+	check = open(thisPath)
+	good = True
+	try: 
+		if str(check.read()).startswith('<'):
+			good = False
+	except Exception as e:
+		pass
 
-	for track in tracks:
-		if track.name != '' and not track.name.startswith("Percussion"):
-			newMidi = MidiFile()
-			newMidi.tracks.append(track)
-			if not os.path.exists(patha[:-7] +'solo\\'+track.name):
-   				os.makedirs(patha[:-7] +'solo\\'+track.name)
-   				print("making directory: ", 'solo\\'+track.name)
-			newMidi.save(patha[:-7] +'solo\\'+track.name+'\\'+fileN[:-4]+'_'+track.name+'.mid')
-			print("saved...")
-			print(patha[:-7] +'solo\\'+track.name+'\\'+fileN[:-4]+'_'+track.name+'.mid')
+
+	if good:
+		
+		thisfile = MidiFile(thisPath, clip = True)
+		for track in thisfile.tracks:
+		    tracks.append(track)
+
+		for track in tracks:
+			if track.name != '' and not track.name.startswith("Percussion"):
+				newMidi = MidiFile()
+				newMidi.tracks.append(track)
+				if not os.path.exists(patha[:-7] +'solo\\'+track.name):
+	   				os.makedirs(patha[:-7] +'solo\\'+track.name)
+	   				print("making directory: ", 'solo\\'+track.name)
+				newMidi.save(patha[:-7] +'solo\\'+track.name+'\\'+fileN[:-4]+'_'+track.name+'.mid')
+				print("saved...")
+				print(patha[:-7] +'solo\\'+track.name+'\\'+fileN[:-4]+'_'+track.name+'.mid')
 
 
